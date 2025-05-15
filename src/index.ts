946c13eb6265385d387a872520782eeed00c2122
antosnizzah <antonygatitu327@gmail.com>
2025-05-15 10:27:30 +0300

    solved merge conflict in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import { libraryImplementation } from "./library";
+import { EcommerceImplementation } from "./ecommerce";
+import RideSystem from './RideSystem';
+import { zoo } from "./zoo";
+libraryImplementation();
+EcommerceImplementation();
+RideSystem();
+zoo();
